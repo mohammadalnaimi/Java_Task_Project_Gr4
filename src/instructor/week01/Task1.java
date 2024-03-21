b277340250cfb1281fc3aae8b7781a99e6b4fcf7
Owen <owen.constantine@cydeo.com>
2024-03-22 00:35:14 +0300

    First Tasks were on the way

--- src/instructor/week01/Task1.java
+++ src/instructor/week01/Task1.java
@@ -1,4 +1,9 @@
 package instructor.week01;
+/*
+     QUESTION 1 - Write a method which can identify if a given number odd or even
+      Ex: identify(5) -->"Odd"
+      identify (6) --> "Even"
+     */
 
 public class Task1 {
 }
