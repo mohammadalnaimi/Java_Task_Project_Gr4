d00bb2b45efe2e8a29b308cf6dc98341823d02af
Aisulu <aisrysbekv@gmail.com>
2024-03-21 21:18:54 -0500

    Posted solution for task1 and task2

--- src/aisulu/week01/Task2.java
+++ src/aisulu/week01/Task2.java
@@ -1,4 +1,19 @@
 package aisulu.week01;
 
 public class Task2 {
+
+    public static void main(String[] args) {
+
+        divide(10,5);
+        divide(14,4);
+
+    }
+
+    public static void divide (double num1, double num2){
+        System.out.println(num1 / num2);
+    }
 }
+/*
+    QUESTION 2- Numbers without / operator
+    Write a method that can divide two numbers without using division operator
+     */
