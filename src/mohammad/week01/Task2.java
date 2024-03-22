8821be8a5514ddc93dd0e412446ff381cef29b6b
mohammadalnaimi <alnaimijo88@gmail.com>
2024-03-22 15:43:18 -0500

    First Commit

--- src/mohammad/week01/Task2.java
+++ src/mohammad/week01/Task2.java
@@ -1,4 +1,32 @@
 package mohammad.week01;
+/*
+    QUESTION 2- Numbers without / operator
+    Write a method that can divide two nigative numbers without using division operator
+     */
+
 
 public class Task2 {
+    public static void main(String[] args) {
+        int a =95;
+        int b = 10;
+        divide(a,b);
+    }
+    public static void divide(int a, int b) {
+        if ( a == 0 ){
+            System.out.println(0);
+        } else if (b == 0) {
+            System.out.println("cann't divide by 0");
+
+        } else if (a == b ) {
+            System.out.println(1);
+        } else if (a > b) {
+            int division = 0;
+            while (a >= b){
+                a= a-b;
+                division++;
+            }
+            System.out.println(division);
+        }
+
+    }
 }
--- src/mohammad/week01/Task3.java
+++ src/mohammad/week01/Task3.java
@@ -1,4 +1,24 @@
 package mohammad.week01;
 
+/*
+   QUESTION 3- Write a function which prints out the numbers from 1 to 30 but
+   for numbers which are a multiple of 3, print "FIN" instead of the number
+   and for numbers which are a multiple of 5, print "RA" instead of the number.
+   for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+    */
+
 public class Task3 {
+    public static void main(String[] args) {
+        for (int x = 1 ; x <= 30 ; x++){
+            if (x % 3 == 0 && x % 5 == 0){
+                System.out.print("FINRA\t");
+            } else if (x % 3 == 0) {
+                System.out.print("FIN\t");
+            } else if (x % 5 == 0) {
+                System.out.print("RA\t");
+            }else {
+                System.out.print(x+"\t");
+            }
+        }
+    }
 }
