a56ca8188f9d9f829455d7e75db68e2d212968dc
ridwanalkhafagi <ridwanalkhafagi@gmail.com>
2024-03-28 13:10:37 -0500

    Created conflict package

--- src/ridwan/Task1.java
+++ src/ridwan/Task1.java
@@ -1,4 +1,18 @@
 package ridwan;
 
 public class Task1 {
+    public static void main(String[] args) {
+
+        String answer = identify(-126);
+        System.out.println(answer);
+
+    }
+    public static String identify(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+
+    }
 }
