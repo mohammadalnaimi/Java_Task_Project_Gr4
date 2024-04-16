6a1107c45fcd2016f7a36b6d13b114c9db4bb7c4
Owen <owen.constantine@cydeo.com>
2024-04-16 19:52:55 +0300

    Week 3 Solutions and Week 4 Questions

---
+++ src/instructor/week04/W4_RemoveDuplicates.java
@@ -0,0 +1,11 @@
+package instructor.week04;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class W4_RemoveDuplicates {
+  //  Write a return method that can remove the duplicated values from String
+  //  Ex:  removeDup("AAABBBCCC")  ==> ABC
+
+
+}
