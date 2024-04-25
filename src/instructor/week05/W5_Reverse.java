8076fd5c591b8381f40569073da147ae6f9de0bb
mohammadalnaimi <alnaimijo88@gmail.com>
2024-04-25 15:57:49 -0500

    commit for second week tasks

--- src/instructor/week05/W5_Reverse.java
+++ src/instructor/week05/W5_Reverse.java
@@ -5,6 +5,30 @@ package instructor.week05;
 /* Write a return method that can reverse  String
    Ex: Reverse("ABCD"); ==> DCBA*/
 public class W5_Reverse {
+    public static void main(String[] args) {
 
+        String text ="ABCD";
+        System.out.println(reversed(text));
+        System.out.println(reversed2(text));
+
+    }
+
+    public static StringBuilder reversed(String text){
+
+        StringBuilder sb = new StringBuilder(text);
+
+        return sb.reverse();
+
+    }
+
+    public static String reversed2(String text){
+
+        String reversed ="";
+        for (int i = text.length()-1; i >= 0 ; i--) {
+            reversed+=text.charAt(i);
+        }
+        return reversed;
+
+    }
 
 }
