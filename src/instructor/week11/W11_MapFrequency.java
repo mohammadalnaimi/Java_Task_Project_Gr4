a538ab20b9c157dc47439fb05895fd5ffe0f3e28
OwenEST <owen.constantine@cydeo.com>
2024-06-14 02:07:39 +0300

    week 10 and 11 package

---
+++ src/instructor/week11/W11_MapFrequency.java
@@ -0,0 +1,10 @@
+package instructor.week11;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+//Write a method that prints the frequency of each character from a String
+public class W11_MapFrequency {
+
+
+}
---
+++ src/instructor/week11/W11_MapMinValue.java
@@ -0,0 +1,15 @@
+package instructor.week11;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+//Write a method that can return the minimum value from ta map (DO NOT use sort method)
+public class W11_MapMinValue {
+
+
+
+
+
+
+}
